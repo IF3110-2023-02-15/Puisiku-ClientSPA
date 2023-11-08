f28ffe1f0dddf44e194da47bcd1e0915dd78ae74
auliamey <auliaannandya@gmail.com>
2023-11-08 15:56:28 +0700

    retrieve poem to home

---
+++ src/components/poem/poem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface PoemProps {
+  poem: {
+    id: number;
+    title: string;
+    creator: string;
+  };
+  handlePoemClick: (id: number) => void;
+}
+
+const Poem: React.FC<PoemProps> = ({ poem, handlePoemClick }) => {
+  console.log(poem);
+  return (
+    <div
+      key={poem.id}
+      onClick={() => handlePoemClick(poem.id)}
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "10px",
+        marginBottom: "10px",
+        cursor: "pointer",
+      }}
+    >
+      <h2>{poem.title}</h2>
+      <p>{poem.creator}</p>
+    </div>
+  );
+};
+
+export default Poem;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,54 @@
 import { useAuth } from "@/contexts/AuthContext";
+import { useState, useEffect } from "react";
 import { Button, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { getPoems } from "@/api";
+import Poem from "../../components/poem/poem"
+
+interface IPoem {
+  id: number;
+  title: string;
+  creator: string;
+}
 
 const Home = () => {
   const { id } = useAuth();
+  const [poems, setPoems] = useState<IPoem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getPoems();
+        setPoems(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handlePoemClick = (id: number) => {
+    // Handle click event for each poem box
+    console.log(`Poem with id ${id} clicked.`);
+  };
+
+  console.log(poems);
+
   return (
     <Box>
       <h1>Home {id}</h1>
+      {poems.map((poem) => (
+        <Poem key={poem.id} poem={poem} handlePoemClick={handlePoemClick} />
+      ))}
       <Button as={Link} to="/profile" colorScheme="pink" width="fit-content">
         Profile
+      </Button>
+      <Button as={Link} to="/addpoem" colorScheme="pink" width="fit-content">
+        addpoem
       </Button>
     </Box>
   );
 };
 
 export default Home;
+
