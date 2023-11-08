670dd35d00ccfb51d9cf331422d68af6ca8dcef1
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-08 15:59:38 +0700

    chore: save imagepath in local storage and check expired token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,11 +7,14 @@ import React, {
 } from "react";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 interface AuthContextProps {
   id: string;
+  imagePath: string;
   login: (token: string) => void;
   logout: () => void;
+  updateProfileImage: (newImagePath: string) => void;
   loading: boolean;
 }
 
@@ -23,38 +26,62 @@ interface AuthProviderProps {
 
 interface DecodedToken {
   id: string;
+  exp: number;
+  imagePath: string;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const navigate = useNavigate();
+
   const [id, setId] = useState("");
+  const [imagePath, setImagePath] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const storedImagePath = localStorage.getItem("imagePath");
     if (token) {
-      const decodedToken = jwtDecode<DecodedToken>(token);
-      setId(decodedToken.id);
+      const decodedToken: DecodedToken = jwtDecode(token);
+      const currentTime = Date.now().valueOf() / 1000;
+      if (decodedToken.exp < currentTime) {
+        localStorage.removeItem("token");
+        setId("");
+        setImagePath("");
+        navigate("/login");
+      } else {
+        setId(decodedToken.id);
+        setImagePath(storedImagePath || decodedToken.imagePath);
+      }
     }
     setLoading(false);
-  }, []);
+  }, [navigate]);
 
   const login = (token: string) => {
     axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-    console.log(axios.defaults.headers.common);
 
     const decodedToken = jwtDecode<DecodedToken>(token);
     setId(decodedToken.id);
+    setImagePath(decodedToken.imagePath);
 
     localStorage.setItem("token", token);
   };
 
   const logout = () => {
     setId("");
+    setImagePath("");
     localStorage.removeItem("token");
+    localStorage.removeItem("imagePath");
+  };
+
+  const updateProfileImage = (newImagePath: string) => {
+    setImagePath(newImagePath);
+    localStorage.setItem("imagePath", newImagePath);
   };
 
   return (
-    <AuthContext.Provider value={{ id, login, logout, loading }}>
+    <AuthContext.Provider
+      value={{ id, imagePath, login, logout, updateProfileImage, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
