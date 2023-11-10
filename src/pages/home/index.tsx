4895af31456da02824af254d6272fc09fb1005d9
auliamey <auliaannandya@gmail.com>
2023-11-10 13:08:04 +0700

    add home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { Button, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { getPoems } from "@/api";
 import Poem from "../../components/poem/poem"
+import AddPoem from "@/pages/addpoem";
 
 interface IPoem {
   id: number;
@@ -14,6 +15,9 @@ interface IPoem {
 const Home = () => {
   const { id } = useAuth();
   const [poems, setPoems] = useState<IPoem[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClose = () => setIsOpen(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,27 +32,27 @@ const Home = () => {
   }, []);
 
   const handlePoemClick = (id: number) => {
-    // Handle click event for each poem box
     console.log(`Poem with id ${id} clicked.`);
   };
-
-  console.log(poems);
 
   return (
     <Box>
       <h1>Home {id}</h1>
       {poems.map((poem) => (
-        <Poem key={poem.id} poem={poem} handlePoemClick={handlePoemClick} />
+        <Link to={`/poem/${poem.id}`} key={poem.id}>
+          <Poem poem={poem} handlePoemClick={handlePoemClick} />
+        </Link>
       ))}
       <Button as={Link} to="/profile" colorScheme="pink" width="fit-content">
         Profile
       </Button>
-      <Button as={Link} to="/addpoem" colorScheme="pink" width="fit-content">
-        addpoem
+      <Button onClick={() => setIsOpen(true)} colorScheme="pink" width="fit-content">
+        Add Poem
       </Button>
+
+      <AddPoem isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 };
 
 export default Home;
-
