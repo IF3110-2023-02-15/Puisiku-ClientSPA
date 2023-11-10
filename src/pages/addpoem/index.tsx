4730f5577403738823ee5ea594851699e2b95f02
auliamey <auliaannandya@gmail.com>
2023-11-10 13:08:58 +0700

    refactor add poem page to modal

--- src/pages/addpoem/index.tsx
+++ src/pages/addpoem/index.tsx
@@ -1,25 +1,78 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
 import { addPoem } from '@/api';
-// import { Link } from "react-router-dom";
-// import { Button } from '@chakra-ui/react';
+import { uploadImageFile, uploadAudioFile } from "@/api";
+import { useAuth } from "@/contexts/AuthContext";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Input,
+  Textarea,
+  Select,
+} from '@chakra-ui/react';
 
-const AddPoem = () => {
+interface AddPoemModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AddPoem: React.FC<AddPoemModalProps> = ({ isOpen, onClose }) => {
+  const { id } = useAuth();
+
   const [formData, setFormData] = useState({
     title: '',
     content: '',
     genre: '',
     image: null,
     audio: null,
-    year: '',
+    year: 2023,
   });
-  
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      await addPoem(formData);
-      console.log("tes");
+      let newFormData = { ...formData , creatorId : id};
+      const imageFile = formData.image;
+      const audioFile = formData.audio;
+
+      if (imageFile) {
+        const formData = new FormData();
+        formData.append("file", imageFile);
+
+        const imagePath = await uploadImageFile(formData);
+
+        const { filePath } = imagePath;
+
+        if (!filePath) {
+          throw new Error("Image path is missing");
+        }
+
+        newFormData = { ...newFormData, image: filePath };
+      }
+
+      if (audioFile) {
+        const audioFormData = new FormData();
+        audioFormData.append("file", audioFile);
+  
+        const audioPath = await uploadAudioFile(audioFormData);
+  
+        const { filePath } = audioPath;
+  
+        if (!filePath) {
+          throw new Error("Audio path is missing");
+        }
+  
+        newFormData = { ...newFormData, audio: filePath};
+      }
+
+      await addPoem(newFormData);
+      
     } catch (error) {
       console.error("Error adding poem", error);
     }
@@ -38,40 +91,54 @@ const AddPoem = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Title</label>
-        <input type="text" name="title" value={formData.title} onChange={handleInputChange} />
-      </div>
-      <div>
-        <label>Content</label>
-        <textarea name="content" value={formData.content} onChange={handleInputChange} />
-      </div>
-      <div>
-        <label>Genre</label>
-        <select name="genre" value={formData.genre} onChange={handleInputChange}>
-          <option value="Romantic">Romantic</option>
-          <option value="Patriot">Patriot</option>
-          <option value="Eligi">Eligi</option>
-          <option value="Education">Education</option>
-          <option value="Nature">Nature</option>
-          <option value="Teacher">Teacher</option>
-        </select>
-      </div>
-      <div>
-        <label>Image</label>
-        <input type="file" name="image" accept=".jpg, .jpeg, .png" onChange={handleFileChange} />
-      </div>
-      <div>
-        <label>Audio</label>
-        <input type="file" name="audio" accept=".mp3" onChange={handleFileChange} />
-      </div>
-      <div>
-        <label>Year</label>
-        <input type="number" name="year" value={formData.year} onChange={handleInputChange} />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit}>
+            <ModalHeader>Add Poem</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            <div>
+              <label>Title</label>
+              <Input type="text" name="title" value={formData.title} onChange={handleInputChange} />
+            </div>
+              <div>
+                <label>Content</label>
+                <Textarea name="content" value={formData.content} onChange={handleInputChange} />
+              </div>
+              <div>
+                <label>Genre</label>
+                <Select name="genre" value={formData.genre} onChange={handleInputChange}>
+                  <option value="Romantic">Romantic</option>
+                  <option value="Patriot">Patriot</option>
+                  <option value="Eligi">Eligi</option>
+                  <option value="Education">Education</option>
+                  <option value="Nature">Nature</option>
+                  <option value="Teacher">Teacher</option>
+                </Select>
+              </div>
+              <div>
+                <label>Image</label>
+                <Input type="file" name="image" accept=".jpg, .jpeg, .png" onChange={handleFileChange} />
+              </div>
+              <div>
+                <label>Audio</label>
+                <Input type="file" name="audio" accept=".mp3" onChange={handleFileChange} />
+              </div>
+              <div>
+                <label>Year</label>
+                <Input type="number" name="year" value={formData.year} onChange={handleInputChange} />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Submit
+              </Button>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
   );
 };
 
