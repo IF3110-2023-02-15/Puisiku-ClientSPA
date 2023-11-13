ce1435ae74a0fd0bb795f7803ef9b9eb12e3f896
auliamey <auliaannandya@gmail.com>
2023-11-14 00:10:45 +0700

    fix poem component

--- src/components/poem/poem.tsx
+++ src/components/poem/poem.tsx
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
 
 interface PoemProps {
   poem: {
     id: number;
     title: string;
-    creator: string;
+    creatorId: string;
+    genre: string;
   };
   handlePoemClick: (id: number) => void;
 }
 
 const Poem: React.FC<PoemProps> = ({ poem, handlePoemClick }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div
+    <Box
       key={poem.id}
       onClick={() => handlePoemClick(poem.id)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      display="flex"
+      justifyContent="space-between"
       style={{
         border: "1px solid #ccc",
-        borderRadius: "5px",
+        width: "60%",
         padding: "10px",
-        marginBottom: "10px",
+        borderRadius: "5px",
         cursor: "pointer",
+        transition: "transform 0.3s ease-in-out",
+        transform: isHovered ? "scale(1.03)" : "scale(1)",
+        backgroundColor: isHovered ? "#88E2D4" : "#FFFFFF",
       }}
     >
-      <h2>{poem.title}</h2>
-      <p>{poem.creator}</p>
-    </div>
+      <Text as='b' textDecoration={isHovered ? "underline" : "none"}>
+        {poem.title}
+      </Text>
+      <Text as='i' color="#AEAEAE">
+        {poem.genre}
+      </Text>
+    </Box>
   );
 };
 
