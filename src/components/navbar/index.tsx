7146c1d56dcb1eeeb8743a2615330cef7e996d9c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-15 18:01:22 +0700

    refactor: use const from config

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,8 +2,7 @@ import { Flex, Heading, Avatar, Spacer } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import Loading from "../loading";
-
-const REST_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
+import { REST_BASE_URL } from "@/configs/config.ts";
 
 const Navbar = () => {
   const { imagePath, loading } = useAuth();
@@ -12,6 +11,7 @@ const Navbar = () => {
     return <Loading />;
   }
 
+  const imageUrl = REST_BASE_URL + imagePath
   return (
     <Flex w="100%" px={8} py={2} className="shadow-md">
       <Link to="/home">
@@ -22,7 +22,7 @@ const Navbar = () => {
         <Avatar
           borderRadius="full"
           boxSize="50px"
-          src={REST_BASE_URL + imagePath}
+          src={imageUrl}
           name="Profile"
         />
       </Link>
