665a25825d2a82cacb502b374597d02875b0c056
auliamey <auliaannandya@gmail.com>
2023-11-17 01:04:39 +0700

    style error text

--- src/pages/errors/401.tsx
+++ src/pages/errors/401.tsx
@@ -1,5 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
 const Unauthorized = () => {
-  return <h1>Unauthorized</h1>;
+  return <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+    <Text as='b' fontSize="42px">Unauthorized</Text>
+  </Box>
 };
 
 export default Unauthorized;
--- src/pages/errors/404.tsx
+++ src/pages/errors/404.tsx
@@ -1,5 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+    <Text as='b' fontSize="42px">Not Found</Text>
+  </Box>
 };
 
 export default NotFound;
