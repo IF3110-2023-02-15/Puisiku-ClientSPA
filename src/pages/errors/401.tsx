a44233a58760f8c53effb5e3ea0d1f86cf54954a
tubagusrahardi <tubagusrd25@gmail.com>
2023-11-17 14:08:22 +0700

    New Page 401

--- src/pages/errors/401.tsx
+++ src/pages/errors/401.tsx
@@ -1,5 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Unauthorized = () => {
-  return <h1>Unauthorized</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center  ">
+      <h1 className="text-9xl font-medium text-center ">401</h1>
+      <p className="text-center">Sorry! Unauthorized</p>
+      <div className=" flex justify-center ">
+        <Link to="/">
+          <button className=" justify-center align-items:center bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
+            Back To Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Unauthorized;
